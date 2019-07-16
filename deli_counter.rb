084eed5f8450b5981d382186fd684d12c5d89c86
Tracie Masek <traciemasek@gmail.com>
2019-07-16 09:39:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+deli_queue = []
+
+def line(line)
+  new_line_arr = []
+  if line.length == 0 
+    puts "The line is currently empty."
+  else 
+    line.each_with_index do |person, index|
+      new_line_arr << "#{index + 1}. #{person}"
+    end
+    puts "The line is currently: #{new_line_arr.join(" ")}"
+  end
+end
